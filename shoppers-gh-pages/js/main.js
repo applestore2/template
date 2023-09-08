2426b6643d7d29a23afefd6feaa46d374d7c8508
applestore2 <henag7391@naver.com>
2023-09-08 13:30:36 +0900

    회원가입폼, 배너 위치

--- shoppers-gh-pages/js/main.js
+++ shoppers-gh-pages/js/main.js
@@ -270,4 +270,21 @@ jQuery(document).ready(function($) {
 			$('.userInfo').show()
 		})
 	});
+
+	$(function(){
+		$('.zipSearch').click(function execDaumPostcode() {
+				new daum.Postcode( {
+				  oncomplete: function( data ) {
+					document.getElementById( 'zip-code' ).value = data.zonecode;
+					document.getElementById( 'address-1' ).value = data.address;
+				  }
+				} ).open();
+			})
+	});
+
+	$(function(){
+		$('.membership').click(function(){
+			$('.loginBg').show()
+		})
+	});
 });
